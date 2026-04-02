e30cee2f0ce53a64eee42379969a938f0449206a
sebadew <sc8232944@gmail.com>
2026-04-01 22:18:54 -0300

    ejercicio 3

--- Semana5/ejercicio3.py
+++ Semana5/ejercicio3.py
@@ -6,8 +6,19 @@ Debe implementar una versión iterativa y una recursiva.
 """
 
 def factorial_ciclo(n):
-    pass
+    resultado = 1
+    for i in range(1,n+1):
+        resultado = resultado * i
+    return resultado
 
+numero1 = int(input("Ingrese numero: "))
+print(factorial_ciclo(numero1))
 
 def factorial_recursivo(n):
-    pass
+    if n == 1:
+        return 1
+    else:
+        return factorial_recursivo(n-1)*n
+
+numero2 = int(input("Ingrese numero: "))
+print(factorial_recursivo(numero2))
