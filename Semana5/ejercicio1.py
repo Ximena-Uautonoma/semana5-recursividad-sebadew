7a9e567bd42a4e12cd3b9aa1ab2c19998968256a
sebadew <sc8232944@gmail.com>
2026-04-01 21:57:25 -0300

    ejercicio 1 listo

--- Semana5/ejercicio1.py
+++ Semana5/ejercicio1.py
@@ -6,17 +6,31 @@ Debe implementar dos funciones:
 2. Una usando recursividad
 """
 
-def contar_ciclo(n):
+def contar_ciclo(n,lista):
     """
     Retorna una lista con los números desde 1 hasta n usando iteración.
     """
     # Escriba aquí su solución y borre la palabra pass de acontinuación
-    pass
+    for i in range(n):
+        lista.append(i+1)
 
+numeros = []
+n = int(input("Ingrese hasta que numero llegue la lista:   "))
+contar_ciclo(n,numeros)
+print(numeros)
 
-def contar_recursivo(n):
+def contar_recursivo(n,lista):
     """
     Retorna una lista con los números desde 1 hasta n usando recursividad.
     """
     # Escriba aquí su solución y borre la palabra pass de acontinuación
-    pass
+    if n == 0:
+        return
+    else:
+        lista.append(n)
+        contar_recursivo(n-1,lista)
+
+lista1 = []
+n1 = int(input("Ingrese un numero:  "))
+contar_recursivo(n1,lista1)
+print(lista1)
